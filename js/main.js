6bd49bc75f18d6850e70e52ac9859f24172407bd
masha.khokhlova <zisternov@gmail.com>
2018-03-13 16:35:10 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,139 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        anchors: ['page1', 'page2', 'page3', 'page4'],
+        css3: true,
+        slidesNavigation: true,
+        navigationTooltips: ['HOME', 'ABOUT', 'PROJECTS', 'CONTACT'],
+        controlArrows: false,
+        // afterRender: function () {
+        //     setInterval(function () {
+        //         $.fn.fullpage.moveSlideRight();
+        //     }, 10000);
+        // },
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 1) {
+                $('.header').addClass('fixed');
+            }
+            if (nextIndex == 1) {
+                $('.header').removeClass('fixed');
+            }
+        }
+    });
+
+    // Particles config
+    particlesJS('particles-js',
+        {
+            "particles": {
+                "number": {
+                    "value": 120,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#1992c3"
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#3E557C"
+                    },
+                    "polygon": {
+                        "nb_sides": 3
+                    },
+                    "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 8,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 50,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#1992c3",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 4,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                    },
+                    "resize": true,
+                    "onresize": {
+                        "enable": true,
+                        "density_auto": true,
+                        "density_area": 400
+                    }
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true
+        }
+    );
+});
